5cec08b8f59e3891f7281999180311a5d5486857
Ali <apple@Apples-iMac.local>
2024-08-04 20:05:24 +0330

    Initial commit: Django Todo app

---
+++ todo_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Todo
+from .forms import TodoForm
+
+@login_required
+def todo_list(request):
+    todos = Todo.objects.filter(user=request.user)
+    form = TodoForm()
+    return render(request, 'todo_app/todo_list.html', {'todos': todos, 'form': form})
+
+@login_required
+def add_todo(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.user = request.user
+            todo.save()
+    return redirect('todo_list')
+
+@login_required
+def toggle_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    todo.completed = not todo.completed
+    todo.save()
+    return redirect('todo_list')
+
+@login_required
+def delete_todo(request, pk):
+    Todo.objects.get(pk=pk).delete()
+    return redirect('todo_list')
